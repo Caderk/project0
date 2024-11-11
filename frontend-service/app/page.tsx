8ea1164c9aef7ff6cab79a9b54971a450844c140
Carlos Radtke <carlos.radtke.a@gmail.com>
2024-11-11 18:00:43 -0300

    Turned link into an 'a' element.

--- frontend-service/app/page.tsx
+++ frontend-service/app/page.tsx
@@ -106,7 +106,7 @@ export default function Page() {
     <div className={styles.inventory} style={{ padding: '20px' }}>
       <h1>Welcome to my personal project site!</h1>
       <p>Currently, this page showcases a prototype of my inventory-service implementation. It&apos;s temporarily set up as the website&apos;s main content while I work on designing a dedicated homepage.
-        Feel free to explore and interact with the service, or check out the API directly here: https://caderk.ddns.net/inventory-service.</p>
+        Feel free to explore and interact with the service, or check out the API directly here: <a href="https://caderk.ddns.net/inventory-service">https://caderk.ddns.net/inventory-service</a>.</p>
 
       <h1>Real-Time Items List</h1>
 
