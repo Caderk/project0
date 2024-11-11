b510281c0ce6d28eb453947de662abb78e5ddaba
Carlos Radtke <carlos.radtke.a@gmail.com>
2024-11-11 17:56:35 -0300

    Added a message describing the current state of the webpage

--- frontend-service/app/page.tsx
+++ frontend-service/app/page.tsx
@@ -104,6 +104,10 @@ export default function Page() {
 
   return (
     <div className={styles.inventory} style={{ padding: '20px' }}>
+      <h1>Welcome to my personal project site!</h1>
+      <p>Currently, this page showcases a prototype of my inventory-service implementation. It&apos;s temporarily set up as the website&apos;s main content while I work on designing a dedicated homepage.
+        Feel free to explore and interact with the service, or check out the API directly here: https://caderk.ddns.net/inventory-service.</p>
+
       <h1>Real-Time Items List</h1>
 
       {/* Add New Item */}
