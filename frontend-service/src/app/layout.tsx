e75cc31e5ea8fd3ffb20083b053d4213d3ff7d4f
Carlos Radtke <carlos.radtke.a@gmail.com>
2024-11-12 23:40:42 -0300

    First aproach to a responsive layout

--- frontend-service/src/app/layout.tsx
+++ frontend-service/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Roboto } from "next/font/google"
 
-const roboto = Roboto({ weight: "400", subsets: ["latin"]});
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -10,7 +10,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        <div className="container">
+          <div className="item-1"></div>
+          <div className="item-2"></div>
+          <div className="item-3">
+            {children}
+          </div>
+          <div className="item-4"></div>
+          <div className="item-5"></div>
+        </div>
+      </body>
     </html>
   )
 }
