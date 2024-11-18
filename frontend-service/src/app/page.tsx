84553d8d63eb336d7659d61a29a7382ec6832962
Carlos Radtke <carlos.radtke.a@gmail.com>
2024-11-18 01:09:07 -0300

    Changed some text to be more descriptive.

--- frontend-service/src/app/page.tsx
+++ frontend-service/src/app/page.tsx
@@ -6,10 +6,9 @@ export default function Page() {
     <>
       <h1>Welcome to my personal project site!</h1>
       <p>
-        Currently, this page showcases a prototype of my inventory-service implementation. It&apos;s
-        temporarily set up as the website&apos;s main content while I work on designing a dedicated
-        homepage. Feel free to explore and interact with the service, or check out the API directly
-        here: <a href="https://caderk.ddns.net/inventory-service">Inventory Service API</a>.
+        This project is a full-stack web application designed to showcase personal projects and demonstrate proficiency in modern web development technologies. It features a responsive frontend built with Next.js and TypeScript, it interacts with multiple backend services, and is fully containerized using Docker.
+        
+        I&apos;m currently working on the layout, palettes, and UX design of the site.
       </p>
     </>
   );
