f4d0067b0952151a7f45e2fdf30f318fb64be3f1
Carlos Radtke <carlos.radtke.a@gmail.com>
2024-11-19 23:35:17 -0300

    Wording and linebreaks.

--- frontend-service/src/app/page.tsx
+++ frontend-service/src/app/page.tsx
@@ -1,14 +1,23 @@
 // app/page.tsx
-'use client';
+"use client";
 
 export default function Page() {
   return (
     <>
       <h1>Welcome to my personal project site!</h1>
       <p>
-        This project is a full-stack web application designed to showcase personal projects and demonstrate proficiency in modern web development technologies. It features a responsive frontend built with Next.js and TypeScript, it interacts with multiple backend services, and is fully containerized using Docker.
-        
-        I&apos;m currently working on the layout, palettes, and UX design of the site.
+        This project is a full-stack web application designed to showcase
+        personal projects and demonstrate proficiency in modern web development
+        technologies.
+      </p>
+      <p>
+        It features a responsive frontend built with Next.js and TypeScript, it
+        interacts with multiple backend services, and is fully containerized
+        using Docker.
+      </p>
+      <p>
+        I&apos;m currently working on the layout, palettes, and UX design of the
+        site.
       </p>
     </>
   );
