5d3e505ed2a5fcf8b4e07634af1a40be9a997917
Carlos Radtke <carlos.radtke.a@gmail.com>
2024-12-01 15:54:38 -0300

    Moved CNN model to its own package

--- emotion-detection-service/train.py
+++ emotion-detection-service/train.py
@@ -1,89 +1,13 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F  # Import torch.nn.functional as F
 import torch.optim as optim
 from tqdm import tqdm
+from models.emotionCNN import EmotionCNN
 
 from preprocessing import train_loader, validation_loader
 
 # Define device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-# Model
-class EmotionCNN(nn.Module):
-    def __init__(self):
-        super(EmotionCNN, self).__init__()
-        # First Convolution Block
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3)
-        self.bn1 = nn.BatchNorm2d(32)
-
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.pool1 = nn.MaxPool2d(kernel_size=2)
-        self.dropout1 = nn.Dropout(0.25)
-
-        # Second Convolution Block
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
-        self.bn3 = nn.BatchNorm2d(128)
-
-        self.conv4 = nn.Conv2d(128, 128, kernel_size=3)
-        self.bn4 = nn.BatchNorm2d(128)
-        self.pool2 = nn.MaxPool2d(kernel_size=2)
-        self.dropout2 = nn.Dropout(0.25)
-
-        # Third Convolution Block
-        self.conv5 = nn.Conv2d(128, 256, kernel_size=3)
-        self.bn5 = nn.BatchNorm2d(256)
-
-        self.conv6 = nn.Conv2d(256, 256, kernel_size=3)
-        self.bn6 = nn.BatchNorm2d(256)
-        self.pool3 = nn.MaxPool2d(kernel_size=2)
-        self.dropout3 = nn.Dropout(0.25)
-
-        # Fully Connected Layers
-        self.fc1 = nn.Linear(256 * 2 * 2, 256)
-        self.bn7 = nn.BatchNorm1d(256)
-        self.dropout_fc = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(256, 7)  # 7 classes for emotion recognition
-
-    def forward(self, x):
-        # First Convolution Block
-        x = F.relu(self.conv1(x))
-        x = self.bn1(x)
-        x = F.relu(self.conv2(x))
-        x = self.bn2(x)
-        x = self.pool1(x)
-        x = self.dropout1(x)
-
-        # Second Convolution Block
-        x = F.relu(self.conv3(x))
-        x = self.bn3(x)
-        x = F.relu(self.conv4(x))
-        x = self.bn4(x)
-        x = self.pool2(x)
-        x = self.dropout2(x)
-
-        # Third Convolution Block
-        x = F.relu(self.conv5(x))
-        x = self.bn5(x)
-        x = F.relu(self.conv6(x))
-        x = self.bn6(x)
-        x = self.pool3(x)
-        x = self.dropout3(x)
-
-        # Flatten
-        x = x.view(x.size(0), -1)
-
-        # Fully Connected Layers
-        x = F.relu(self.fc1(x))
-        x = self.bn7(x)
-        x = self.dropout_fc(x)
-        x = self.fc2(
-            x
-        )  # No activation function here; will apply CrossEntropyLoss which includes LogSoftmax
-        return x
-
 
 model = EmotionCNN().to(device)
 
